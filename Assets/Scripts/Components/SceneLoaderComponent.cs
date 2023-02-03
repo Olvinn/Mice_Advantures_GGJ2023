e4023b7ad088d40adf5b9d461f57784683734cb4
Kirill <91202716+DON-BOLSHOF@users.noreply.github.com>
2023-02-03 19:00:45 +0500

    StartSceneCommit
    Add first scene

---
+++ Assets/Scripts/Components/SceneLoaderComponent.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Components
+{
+    public class SceneLoaderComponent : MonoBehaviour
+    {
+        [SerializeField] private string _sceneName;
+        
+        public void LoadScene()
+        {
+            SceneManager.LoadScene(_sceneName);
+        }
+    }
+}
