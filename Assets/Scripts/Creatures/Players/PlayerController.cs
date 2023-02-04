4dc87663daaa4a48705d0bec010ee6aa8a6b7311
Olvin <paradox_x94@hotmail.com>
2023-02-04 14:23:43 +0300

    - jump mechanic

--- Assets/Scripts/Creatures/Players/PlayerController.cs
+++ Assets/Scripts/Creatures/Players/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 namespace Creatures.Players
@@ -5,13 +6,16 @@ namespace Creatures.Players
     [RequireComponent(typeof(CharacterController))]
     public class PlayerController : Creature
     {
-        [SerializeField] private float _speed, _rotationSpeed;
+        [SerializeField] private float _speed, _rotationSpeed, _jumpPower = 10f;
     
         [SerializeField] private CharacterController _characterController;
         [SerializeField] protected Animator _animator;
         private CreatureState State { get; set; }
 
         private static readonly int Speed = Animator.StringToHash("Speed");
+        private static readonly int Falling = Animator.StringToHash("Falling");
+
+        private Vector3 _fallingSpeed;
 
         private void Rotate(Vector3 dir)
         {
@@ -20,18 +24,13 @@ namespace Creatures.Players
             var cur = Vector3.RotateTowards(transform.forward, dir, _rotationSpeed * Time.deltaTime, 1);
             transform.forward = cur;
         }
-        
 
         private void Update()
         {
+            //get data from last move which was actually about falling
+            bool grounded = _characterController.isGrounded;
+            
             Vector3 dir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-            Move(dir);
-        }
-
-        private void Move(Vector3 dir)
-        {
-            if (State == CreatureState.Attack)
-                return;
             
             Vector3 forwardVector = Camera.main.transform.forward;
             forwardVector.y = 0;
@@ -41,9 +40,37 @@ namespace Creatures.Players
 
             var res = ang * dir;
             
-            _characterController.Move(res * _speed * Time.deltaTime);
+            Move(res);
             Rotate(res);
+
+            if (Input.GetButtonDown("Jump") && grounded)
+            {
+                _fallingSpeed = Vector3.up * _jumpPower;
+                grounded = false;
+            }
+
+            //falling logic
+            if (grounded)
+            {
+                _fallingSpeed = Vector3.down;
+                _characterController.Move(_fallingSpeed);
+            }
+            else
+            {
+                _fallingSpeed += Physics.gravity * Time.deltaTime;
+                _characterController.Move(_fallingSpeed * Time.deltaTime);
+            }
+
             _animator.SetFloat(Speed, res.magnitude * _speed);
+            _animator.SetBool(Falling, !grounded);
+        }
+
+        private void Move(Vector3 dir)
+        {
+            if (State == CreatureState.Attack)
+                return;
+            
+            _characterController.Move(dir * (_speed * Time.deltaTime));
         }
 
         public override void Attack()
