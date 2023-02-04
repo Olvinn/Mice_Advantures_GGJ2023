7f0d7c7ee6794b9b69b8f2baeaaad654533b11fb
Olvin <paradox_x94@hotmail.com>
2023-02-04 13:42:04 +0300

    - Added props; Want your hardware to burn

--- Assets/Scripts/Players/PlayerController.cs
+++ Assets/Scripts/Players/PlayerController.cs
@@ -1,65 +1,87 @@
 using UnityEngine;
 
-[RequireComponent(typeof(CharacterController))]
-public class PlayerController : MonoBehaviour
+namespace Players
 {
-    public float speed, rotationSpeed;
+    [RequireComponent(typeof(CharacterController))]
+    public class PlayerController : MonoBehaviour
+    {
+        public float speed, rotationSpeed;
     
-    [SerializeField] private CharacterController characterController;
-    [SerializeField] private Animator animator;
+        [SerializeField] private CharacterController characterController;
+        [SerializeField] private Animator animator;
     
-    public PlayerState state { get; private set; }
+        public PlayerState state { get; private set; }
+        private float _fallSpeed;
 
-    private void Update()
-    {
-        Vector3 dir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        Move(dir);
-    }
+        private void Update()
+        {
+            if (!characterController.isGrounded)
+                state = PlayerState.Falling;
 
-    public void Move(Vector3 dir)
-    {
-        if (state == PlayerState.Attack)
-            return;
+            Vector3 dir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
             
-        Vector3 forwardVector = Camera.main.transform.forward;
-        forwardVector.y = 0;
-        forwardVector.Normalize();
+            Vector3 forwardVector = Camera.main.transform.forward;
+            forwardVector.y = 0;
+            forwardVector.Normalize();
             
-        Quaternion ang = Quaternion.FromToRotation(Vector3.forward, forwardVector);
+            Quaternion ang = Quaternion.FromToRotation(Vector3.forward, forwardVector);
 
-        var res = ang * dir;
+            var res = ang * dir;
             
-        characterController.Move(res * speed * Time.deltaTime);
-        Rotate(res);
-        animator.SetFloat("Speed", res.magnitude * speed);
-    }
+            switch (state)
+            {
+                case PlayerState.Move:
+                    _fallSpeed = 1;
+                    Move(Vector3.down);
+                    Move(res);
+                    Rotate(res);
+                    break;
+                case PlayerState.Falling:
+                    _fallSpeed += Physics.gravity.magnitude * Time.deltaTime;
+                    Move(Vector3.down * _fallSpeed);
+                    Move(res);
+                    Rotate(res);
+                    break;
+            }
+        }
 
-    public void Rotate(Vector3 dir)
-    {
-        if (dir == Vector3.zero)
-            return;
-        Vector3 cur = Vector3.RotateTowards(transform.forward, dir, rotationSpeed * Time.deltaTime, 1);
-        transform.forward = cur;
-    }
+        public void Move(Vector3 dir)
+        {
+            if (state == PlayerState.Attack)
+                return;
+            
+            characterController.Move(dir * (speed * Time.deltaTime));
+            animator.SetFloat("Speed", dir.magnitude * speed);
+        }
 
-    public void Attack()
-    {
+        public void Rotate(Vector3 dir)
+        {
+            if (dir == Vector3.zero)
+                return;
+            Vector3 cur = Vector3.RotateTowards(transform.forward, dir, rotationSpeed * Time.deltaTime, 1);
+            transform.forward = cur;
+        }
+
+        public void Attack()
+        {
         
+        }
+
+#if UNITY_EDITOR
+        private void OnValidate()
+        {
+            if (characterController == null)
+                characterController = GetComponent<CharacterController>();
+            if (animator == null)
+                animator = GetComponentInChildren<Animator>();
+        }
+#endif
     }
 
-    #if UNITY_EDITOR
-    private void OnValidate()
+    public enum PlayerState
     {
-        if (characterController == null)
-            characterController = GetComponent<CharacterController>();
-        if (animator == null)
-            animator = GetComponentInChildren<Animator>();
+        Move,
+        Attack,
+        Falling
     }
-    #endif
-}
-
-public enum PlayerState
-{
-    Move,
-    Attack
 }
