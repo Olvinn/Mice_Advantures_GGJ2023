8c86ef309398c94435ba79b3f950674809ab237e
Olvin <paradox_x94@hotmail.com>
2023-02-05 05:43:13 +0300

    - Now mice death is much dramatic

--- Assets/Scripts/Scenes/Game.cs
+++ Assets/Scripts/Scenes/Game.cs
@@ -1,11 +1,29 @@
+using System;
 using System.Collections;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.HighDefinition;
 using UnityEngine.SceneManagement;
 
 namespace Scenes
 {
     public class Game : MonoBehaviour
     {
+        [SerializeField] private Volume vol;
+        
+        private void Start()
+        {
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+
+        public void UpdatePostProc(float val)
+        {
+            Vignette vig;
+            if (vol.profile.TryGet(out vig))
+                vig.intensity.value = 1 - val;
+        }
+
         public void RestartGame()
         {
             StartCoroutine(Restart());
